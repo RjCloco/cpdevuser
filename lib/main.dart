8eaf90db344f3e0eb90cd2bbd580094a0a3bd8b7
Hxtreme <anandha.kumar@cloco.com.au>
2023-08-31 13:02:48 +0530

    new file

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() async {
   );
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -89,9 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
